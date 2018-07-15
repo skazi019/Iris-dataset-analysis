85270a95d4fd43ad5058f1c6190311730e8dfd7e
Kaushal Sharma <skazi019@gmail.com>
2018-07-15 17:18:24 +0530

    Applying KNearestNeighbors(KNN) Algorithm to the dataset.

--- iris_analysis.py
+++ iris_analysis.py
@@ -3,19 +3,19 @@
 
 # # Importing the iris Dataset.
 
-# In[2]:
+# In[9]:
 
 
 # Loading iris dataset from scikit-learn.
 
 
-# In[3]:
+# In[10]:
 
 
 from sklearn.datasets import load_iris
 
 
-# In[4]:
+# In[11]:
 
 
 # saving 'bunch' object containing iris dataset and its attributes.
@@ -23,7 +23,7 @@ iris = load_iris()
 type(iris)
 
 
-# In[8]:
+# In[6]:
 
 
 # printing iris dataset. (each row represents a flower and each column represents the length and the width)
@@ -31,28 +31,28 @@ print(iris.data)
 iris.data.shape
 
 
-# In[10]:
+# In[12]:
 
 
 # printing name of the four features.
 print(iris.feature_names)
 
 
-# In[11]:
+# In[8]:
 
 
 # printing the integers representing the species of each observation.
 print(iris.target)
 
 
-# In[12]:
+# In[13]:
 
 
 # printing the encoding scheme for species. (0 = setosa, 1 = versicolor, 2 = virginica)
 print(iris.target_names)
 
 
-# In[13]:
+# In[14]:
 
 
 # checking the types of features and response.
@@ -60,14 +60,14 @@ type('iris.data')
 type('iris.target')
 
 
-# In[14]:
+# In[15]:
 
 
 # checking the shape of the features. (first parameter is rows/observations, second parameter is columns/number of features)
 iris.data.shape
 
 
-# In[15]:
+# In[16]:
 
 
 # checking the shape of response. (one parameter matching the nuber of observations)
@@ -76,16 +76,15 @@ iris.target.shape
 
 # # Scatter Plot with iris Dataset.
 
-# In[16]:
+# In[17]:
 
 
 # extracting the values of features and creating a list called featuresALL.
-featuresALL = []
 features = iris.data[:, [0,1,2,3]]
 features.shape
 
 
-# In[17]:
+# In[18]:
 
 
 # extracting the values for target.
@@ -120,7 +119,7 @@ plt.show()
 # ### Scatter Plot with iris Dataset. (Relationship between Sepal Length and Sepal Width) Method #1
 # 
 
-# In[26]:
+# In[23]:
 
 
 # finding relationship between Sepal length and Sepal width.
@@ -146,7 +145,7 @@ plt.show()
 
 # ### Scatter Plot with Iris Dataset (Relationship between Petal length and Petal width) #Method 1
 
-# In[27]:
+# In[24]:
 
 
 # finding relationship between Sepal length and Sepal width.
@@ -172,7 +171,7 @@ plt.show()
 
 # # K - Nearest neighbours (KNN) Algorithm
 
-# In[30]:
+# In[26]:
 
 
 import pandas as pd
@@ -183,7 +182,7 @@ ir['CLASS'] = iris.target
 ir.head() #returns the top 5 rows.
 
 
-# In[32]:
+# In[27]:
 
 
 from sklearn.neighbors import NearestNeighbors
@@ -193,13 +192,13 @@ nn = NearestNeighbors(5) #The arguement specify to return the fast 5 among the
 nn.fit(iris.data) #fitting iris dataset to nearest neighbors algorithm
 
 
-# In[33]:
+# In[28]:
 
 
 ir.describe() #showing the fitted data
 
 
-# In[34]:
+# In[29]:
 
 
 # creating a test data
@@ -209,16 +208,75 @@ test1 = test.reshape(1,-1)
 test1.shape
 
 
-# In[35]:
+# In[30]:
 
 
 nn.kneighbors(test1,5)
 
 
-# In[36]:
+# In[31]:
 
 
 ir.iloc[[98, 93, 57, 60, 79]] # displaying specific rows using iloc() 
 
 
 # ### KNeighborsClassifier Algorithm
+
+# In[38]:
+
+
+import numpy as np
+import matplotlib.pyplot as plt
+from matplotlib.colors import ListedColormap
+from sklearn import neighbors, datasets
+
+n_neighbors = 15
+
+# we only take the first two features. We could avoid this ugly slicing by using 
+# a two-dim dataset
+x = iris.data[:, :2]
+y = iris.target
+
+h = 0.02 #step size in the mesh
+
+#creating color map. cmap maps numbers(which are normalized between [0,1]) to colors.
+cmap_light = ListedColormap(['#FFAAAA', '#AAFFAA', '#AAAAFF']) 
+cmap_bold = ListedColormap(['#FF0000', '#00FF00', '#0000FF'])
+
+for weights in ['uniform', 'distance']:
+    # creating an instance of Neighbors Classifier and fitting the data
+    clf = neighbors.KNeighborsClassifier(n_neighbors, weights=weights)
+    clf.fit(x, y)
+    
+    # for plotting the decision boundary we will assign a color to each point
+    # in the mesh [x_min, x_max]x[y_min, y_max]
+    x_min, x_max = x[:, 0].min() - 1, x[:, 0].max() + 1
+    y_min, y_max = x[:, 1].min() - 1, x[:, 1].max() + 1
+    xx, yy = np.meshgrid(np.arange(x_min, x_max, h), 
+                        np.arange(y_min, y_max, h))
+    z = clf.predict(np.c_[xx.ravel(), yy.ravel()])
+    
+    # putting the result into a colorplot
+    z = z.reshape(xx.shape)
+    plt.figure()
+    plt.pcolormesh(xx, yy, z, cmap=cmap_light)
+    
+    # also plotting training points
+    plt.scatter(x[:, 0], x[:, 1], c=y, cmap=cmap_bold, edgecolor='k', s=20)
+    plt.xlim(xx.min(), xx.max())
+    plt.ylim(yy.min(), yy.max())
+    plt.title("3-Class classification (k = %i, wights = '%s')" 
+              % (n_neighbors, weights))
+    
+plt.show()
+
+
+# ### KNN Classifier Algorithm - Understanding its working
+
+# In[39]:
+
+
+from sklearn.neighbors import KNeighborsClassifier
+knn = KNeighborsClassifier(n_neighbors=1)
+print(knn)
+
